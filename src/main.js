cf9bdf4f41bdfd398eda96dbf40f8344f9be2b12
asifZaman0362 <zero362001@gmail.com>
2020-03-16 14:45:18 +0530

    remove jquery

--- src/main.js
+++ src/main.js
@@ -2,25 +2,28 @@ var isScrolling = false;
 var isShrunk = false;
 var lastScroll = 0;
 
-$(function(){
-    $(window).scroll(function(){
-        if($(this).scrollTop() > 10 && $(this).scrollTop() > lastScroll) {
-            scrollDown();
-        }
-        else if ($(this).scrollTop() <= 0 && $(this).scrollTop() <= lastScroll) {
-            scrollUp();
-        }
-        lastScroll = $(this).scrollTop();
-    });
-});
+// $(function(){
+//     $(window).scroll(function(){
+//         if($(this).scrollTop() > 10 && $(this).scrollTop() > lastScroll) {
+//             scrollDown();
+//         }
+//         else if ($(this).scrollTop() <= 0 && $(this).scrollTop() <= lastScroll) {
+//             scrollUp();
+//         }
+//         lastScroll = $(this).scrollTop();
+//     });
+// });
+
+var body = document.getElementById("body");
+body.style.overflow = "hidden";
 
 function scrollDown() {
     if (isShrunk || isScrolling) return;
     else {
         isShrunk = true;
         isScrolling = true;
-        console.log("scrollDown called.");
         document.getElementsByClassName("static")[0].classList.replace("static", "sticky");
+        body.style.overflow = "auto";
         setTimeout(resetScroll, 2000);
     }
 }
@@ -30,7 +33,8 @@ function scrollUp() {
     else {
         isShrunk = false;
         isScrolling = true;
-        console.log("scrollUp called.");
+        body.style.overflow = "hidden";
+        scrollTo(0, 0);
         document.getElementsByClassName("sticky")[0].classList.replace("sticky", "static");
         setTimeout(resetScroll, 2000);
     }
